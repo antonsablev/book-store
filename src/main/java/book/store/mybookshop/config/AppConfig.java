6b12d5788ab78facd2fbbc0a56d3bdf1b6fa7c8b
antonsablev <live.incntr@gmail.com>
2023-09-03 13:44:54 +0200

    SetUp spring boot, add checkstyle, add Book Model, repository, and service

---
+++ src/main/java/book/store/mybookshop/config/AppConfig.java
@@ -0,0 +1,7 @@
+package book.store.mybookshop.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+}
--- src/main/java/book/store/mybookshop/model/Book.java
+++ src/main/java/book/store/mybookshop/model/Book.java
@@ -6,8 +6,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 import java.math.BigDecimal;
 
+@Getter
+@Setter
 @Entity
 public class Book {
     @Id
@@ -25,59 +29,4 @@ public class Book {
     private String description;
     private String coverImage;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getCoverImage() {
-        return coverImage;
-    }
-
-    public void setCoverImage(String coverImage) {
-        this.coverImage = coverImage;
-    }
 }
