caa726071f5e76220aef4ae0d296e30471b09faf
Anton Sabliev <103369608+antonsablev@users.noreply.github.com>
2023-09-04 20:16:40 +0200

    Revert "Add BookDto.class as a response DTO, and CreateBookRequestDto.class a…"

--- src/main/java/book/store/mybookshop/exception/EntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package book.store.mybookshop.exception;
-
-public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(String message) {
-        super(message);
-    }
-}
